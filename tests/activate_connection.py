91f66856f82cead3faca85a6afd5b49997855805
Rob Baynes <rbaynes@media.mit.edu>
2019-05-17 13:04:23 -0400

    docs

--- tests/activate_connection.py
+++ tests/activate_connection.py
@@ -1,5 +1,17 @@
 # Activate a connection by name
 
+"""
+If you are in the base Balena OS, you have access to nmcli, the NetworkManager
+command line interface.  This is how Rob reconnects to his hidden home wifi
+that his balana cloud image is configured to use:
+
+#!/bin/sh
+nmcli c add type wifi con-name spanky ifname wlan0 ssid spanky
+nmcli con modify spanky wifi-sec.key-mgmt wpa-psk
+nmcli con modify spanky wifi-sec.psk 1sparty0
+nmcli con up spanky
+"""
+
 import NetworkManager
 import sys
 
