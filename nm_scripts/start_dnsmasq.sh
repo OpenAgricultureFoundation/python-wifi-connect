98af089be302eb6f6e20f35427c0f6e41b4330dc
rob baynes <rbaynes@gmail.com>
2019-05-22 18:26:32 +0000

    fixed script bug

--- nm_scripts/start_dnsmasq.sh
+++ nm_scripts/start_dnsmasq.sh
@@ -11,14 +11,18 @@ DEFAULT_DHCP_RANGE="192.168.42.2,192.168.42.254"
 DEFAULT_INTERFACE="wlan0" # use 'ip link show' to see list of interfaces
 
 # run dnsmasq in the background
-dnsmasq "--address=/#/$DEFAULT_GATEWAY "\
-        "--dhcp-range=$DEFAULT_DHCP_RANGE "\
-        "--dhcp-option=option:router,$DEFAULT_GATEWAY "\
-        "--interface=$DEFAULT_INTERFACE "\
-        "--keep-in-foreground "\
-        "--bind-interfaces "\
-        "--except-interface=lo "\
-        "--conf-file "\
-        "--no-hosts " &
+CMD="dnsmasq"\
+' --address=/#/'$DEFAULT_GATEWAY\
+" --dhcp-range=$DEFAULT_DHCP_RANGE"\
+" --dhcp-option=option:router,$DEFAULT_GATEWAY"\
+" --interface=$DEFAULT_INTERFACE"\
+" --keep-in-foreground"\
+" --bind-interfaces"\
+" --except-interface=lo"\
+" --conf-file"\
+" --no-hosts" 
+
+echo "Running in background: $CMD"
+`$CMD` &
 
 
